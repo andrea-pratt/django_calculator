601553e4620e2744f8a96bf4dfd7526a045899f5
Andrea <apratt1013@gmail.com>
2021-05-13 17:29:35 -0500

    migrations

--- calculator/calculator/settings.py
+++ calculator/calculator/settings.py
@@ -76,8 +76,8 @@ TEMPLATES = [
 
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#     'ENGINE': 'django.db.backends.sqlite3',
+#     'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
 
