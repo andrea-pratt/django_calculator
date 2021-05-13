1eaacfefd7798a3e1b217667c80601830d83a01a
Andrea <apratt1013@gmail.com>
2021-05-13 16:13:30 -0500

    add db model

--- calculator/simple_calculator/models.py
+++ calculator/simple_calculator/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.db.models.fields import DateTimeField
 
-# Create your models here.
+
+class Calculation(models.Model):
+    calculation = models.CharField(max_length=200, null=False)
+    creation_date = DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.calculation)
--- calculator/simple_calculator/views.py
+++ calculator/simple_calculator/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from time import sleep
+from .models import Calculation
 import os
 
 
@@ -7,7 +8,7 @@ def calculator(request):
 
     sleep(0.01) # allow time for ajax request to update database before query
 
-    calculations = get_last_10_calculations()
+    calculations = Calculation.objects.all().order_by('-creation_date')[:10]
 
     return render(request, 'simple_calculator/calculator.html', {'calculations': calculations})
 
@@ -21,38 +22,15 @@ def save_calculation(request):
         operator = request.POST.get('operator')
         calculation = f'{first_operand} {operator} {second_operand} = {result}'
 
-        write_calculation(calculation)
-        calculations = get_last_10_calculations()
+        calculation = Calculation(calculation=calculation)
+        calculation.save()
+
+        calculations = Calculation.objects.all().order_by('-creation_date')[:10]
     
     return render(request, 'simple_calculator/calculator.html', {'calculations': calculations})
 
 
-def get_last_10_calculations():
 
-    working_directory = os.getcwd()
-    filename = os.path.join(working_directory, 'simple_calculator', 'calculations.txt')
-
-    with open(filename, 'r') as calc_file:
-        lines = calc_file.readlines()
-
-    calculations = []
- 
-    for i in range(10):
-        try:
-            calculations.append(lines[i*-1])
-        except IndexError:
-            break
-
-    return calculations
-
-
-def write_calculation(calculation):
-
-    working_directory = os.getcwd()
-    filename = os.path.join(working_directory, 'simple_calculator', 'calculations.txt')
-
-    with open(filename, 'a') as calc_file:
-        calc_file.write(calculation + '\n')
 
 
 
