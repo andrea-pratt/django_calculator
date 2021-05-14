b9beb9a5eb5c34dbe8b7a12527eef7b473cb25a3
Andrea <apratt1013@gmail.com>
2021-05-13 19:38:41 -0500

    set up application for local use

--- calculator/calculator/settings.py
+++ calculator/calculator/settings.py
@@ -71,20 +71,20 @@ TEMPLATES = [
 ]
 
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#     'ENGINE': 'django.db.backends.sqlite3',
-#     'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+"""Database configuration used in Heroku deployment"""
+# DATABASES = {
+#     'default': dj_database_url.config()
+# }
 
 
 # Password validation
